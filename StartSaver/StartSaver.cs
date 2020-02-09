3e6492a359a05e0f9fb4d4df832c80dde4d722aa
Ralzar81 <57372363+Ralzar81@users.noreply.github.com>
2020-02-09 10:47:58 +0100

    Add files via upload

---
+++ StartSaver/StartSaver.cs
@@ -0,0 +1,42 @@
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Entity;
+using DaggerfallWorkshop.Game.Utility.ModSupport;
+using UnityEngine;
+using DaggerfallWorkshop.Game.Utility;
+using System;
+
+namespace StartSaver
+{
+
+    public class StartSaver : MonoBehaviour
+    {
+        static Mod mod;
+
+        [Invoke(StateManager.StateTypes.Start, 0)]
+        public static void Init(InitParams initParams)
+        {
+            mod = initParams.Mod;
+            var go = new GameObject(mod.Title);
+            go.AddComponent<StartSaver>();
+            StartGameBehaviour.OnStartGame += StartSaver_OnStartGame;
+        }
+
+        void Awake()
+        {
+            mod.IsReady = true;
+            Debug.Log("[Start Saver] Ready");
+        }
+
+
+        private static void StartSaver_OnStartGame(object sender, EventArgs e)
+        {
+            Debug.Log("[Start Saver] Starting");
+            PlayerEntity playerEntity = GameManager.Instance.PlayerEntity;
+            string saveName = "Start Save";
+            GameManager.Instance.SaveLoadManager.Save(playerEntity.Name, saveName);
+            Debug.Log("[Start Saver] Game Saved");
+        }
+
+
+    }
+}
